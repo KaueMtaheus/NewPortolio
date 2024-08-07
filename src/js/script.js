9f3d207fc461941e20bcd104943211cbcc30b83a
Kauê Matheus <kauematheus7@hotmail.com>
2024-08-07 20:05:36 -0300

    layout

--- src/js/script.js
+++ src/js/script.js
@@ -2,13 +2,13 @@ document.addEventListener('DOMContentLoaded', function () {
   let swiper = new Swiper('.project-slider', {
     effect: 'coverflow',
     grabCursor: true,
-    centeredSlides: true,
+    centeredSlides: false,
     loop: true,
     slidesPerView: 'auto',
     coverflowEffect: {
       rotate: 0,
       stretch: 0,
-      depth: 100,
+      depth: 900,
       modifier: 2.5,
     },
     pagination: {
@@ -25,4 +25,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
 let copy = document.querySelector(".logos-slide").cloneNode(false);
 document.querySelector(".logos").appendChild(copy);
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const elements = [
+      { id: 'development', },
+  ];
+
+  elements.forEach(el => {
+      const element = document.getElementById(el.id);
+      const border = document.createElement('div');
+      border.className = 'animated-border';
+      border.style.borderColor = el.color;
+      element.appendChild(border);
+  });
+});
 
